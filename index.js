d1c62e98cbc2a897a7ba54d23b3c753ac68b0814
takidilmi <angeldilmi@gmail.com>
2023-08-11 18:32:39 +0200

    Compelted assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+// element.style.backgroundColor = "#27647B";
+// element.textContent = "You've changed what's on the screen!";
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+// element.className = "pet-listing dog";
+
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Halla is the champion";
+document.body.appendChild(newHeader);
